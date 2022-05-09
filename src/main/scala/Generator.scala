9e134dd6e2299bdd3b89d58e91b14a31023133d4
sampss <sampsons79@gmail.com>
2022-05-09 15:44:01 -0400

    added sbt and generator.scala

---
+++ src/main/scala/Generator.scala
@@ -0,0 +1,10 @@
+import java.sql.{SQLException, Connection, DriverManager, ResultSet, Statement}
+import scala.collection.mutable.ArrayBuffer
+import scala.io.{BufferedSource, Source}
+import scala.language.postfixOps
+import scala.util.matching.Regex
+import java.sql.ResultSetMetaData
+
+object Generator {
+
+}
