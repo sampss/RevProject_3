b4c6c2ff295b4b82161505119ff8034c2c817b26
maxlandan <40554944+maxlandan@users.noreply.github.com>
2022-05-25 00:46:36 -0400

    Update consumer1.scala

--- src/main/scala/consumer1.scala
+++ src/main/scala/consumer1.scala
@@ -26,6 +26,9 @@ import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 
+
+import scala.io.StdIn.readLine
+
 //Play Libraries for Json
 import play.api.libs.json._
 
@@ -47,7 +50,7 @@ import spark.implicits._
     props.put("auto.offset.reset", "earliest")
     
     val consumer = new KafkaConsumer(props)
-    val topics = List("topictext")
+    val topics = List("test_topic")
     try {
            consumer.subscribe(topics.asJava)
            while (true) {
@@ -56,33 +59,40 @@ import spark.implicits._
 	  	 var jsonSeq = Seq[String]()
                 for (record <- records.asScala) {
 			var recordString:String = record.value()
-			println(s"| Key: ${record.key()} | Value: ${record.value()} | Offset: ${record.offset()} |")
-			var jsonRow:JsValue = Json.parse(recordString)
-			println(jsonRow)
-
-			jsonSeq :+ recordString
+			//println(s"| Key: ${record.key()} | Value: ${record.value()} | Offset: ${record.offset()} |")
+			//println(recordString)
+			//var jsonRow:JsValue = Json.parse(recordString)
+			//println(jsonRow)
+			//println(recordString)
+			//println("----------------------------------")
+			jsonSeq=jsonSeq :+ recordString
+			//println(jsonSeq)
+			//println("------------")
+			println("Number of records : "+jsonSeq.size)
+			//readLine()
            	}
 	      if(jsonSeq.nonEmpty && jsonSeq.size > 100) {
-
+	      	println("-------------------------------------------------------------------------")
 		var jsonDS = jsonSeq.toDS()
 		var df = spark.read.json(jsonDS)
-
-		df = df.withColumn("time", to_timestamp(col("time"), "yyyy-MM-dd HH:mm:ss")).drop("datetime")
-
+		df=df.drop("_corrupt_record")
+		
+//		df = df.withColumn("datetime", to_timestamp(col("time"), "yyyy-MM-dd HH:mm:ss")).drop("datetime")
+		df = df.withColumn("datetime", to_timestamp(col("time"))).drop("time")
 		df.createOrReplaceTempView("orders")
 		var df2 = spark.sql("select * from orders")
 		var props2 = new Properties()
 		props2.put("driver", "org.postgresql.Driver")
 		props2.put("user", username)
 		props2.put("password", password)
-		df2.write.mode("append").jdbc("jdbc:postgresql://localhost:5432/"+database, "orders", props2)
+		df2.write.mode("append").jdbc("jdbc:postgresql://127.0.0.1:5432/"+database, "orders", props2)
 		df.write.format("jdbc")
-		  .option("url", "jdbc:postgresql://localhost:5432"+database)
+		  .option("url", "jdbc:postgresql://127.0.0.1:5432/"+database)
 		  .option("dbtable", "orders")
 		  .option("user", username)
 		  .option("password", password)
 		  .mode("append").save()
-		
+//		println("finale -----------------")
 		jsonSeq = Seq[String]()
 	      }
            	
